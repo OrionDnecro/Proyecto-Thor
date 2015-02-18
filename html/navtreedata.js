2f32b595f5a1969dc768009980df62cae58906a2
Jose Raul <joseph.t7@hotmail.com>
2015-02-18 01:00:42 -0600

    Commit message

---
+++ html/navtreedata.js
@@ -0,0 +1,14 @@
+var NAVTREE =
+[
+  [ "ADeux", "index.html", [
+    [ "README", "md__r_e_a_d_m_e.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click en deshabilitar sincronización';
+var SYNCOFFMSG = 'click en habilitar sincronización';
